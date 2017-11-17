f0cfa7d30809597d9c429d4321fabcf41862e5f5
BuildTools <unconfigured@null.spigotmc.org>
2017-11-17 22:36:34 +0400

    added

--- src/main/java/com/jaysn/bkc/MainClass.java
+++ src/main/java/com/jaysn/bkc/MainClass.java
@@ -2,6 +2,10 @@ package com.jaysn.bkc;
 
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,6 +13,10 @@ public class MainClass extends JavaPlugin {
 
     public static Economy econ = null;
     public static EconomyResponse r;
+    boolean autorespawn;
+    boolean joinmessage;
+    String killmessage;
+    Double killmoney;
 
     public Mechanics mechs;
 
@@ -44,6 +52,10 @@ public class MainClass extends JavaPlugin {
 
     public void loadConfig() {
         getConfig().options().copyDefaults(true);
+        getConfig().set("AllowJoinMessage", joinmessage);
+        getConfig().set("AllowCustomRespawn", autorespawn);
+        getConfig().set("KillMessage", killmessage);
+        getConfig().set("KillMoney", killmoney);
         saveConfig();
     }
 
@@ -61,5 +73,27 @@ public class MainClass extends JavaPlugin {
         }
         econ = rsp.getProvider();
         return econ != null;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("setlobbyspawn") && sender instanceof Player) {
+            Player player = (Player) sender;
+
+            if (!player.hasPermission("bkc.admin")) {
+                player.sendMessage(ChatColor.RED.toString() + ChatColor.BOLD + "You do not have the permission!");
+
+            } else {
+                getConfig().set("LobbySpawn.world", player.getWorld().getName());
+                getConfig().set("LobbySpawn.x", player.getLocation().getX());
+                getConfig().set("LobbySpawn.y", player.getLocation().getY());
+                getConfig().set("LobbySpawn.z", player.getLocation().getZ());
+                getConfig().set("LobbySpawn.yaw", player.getLocation().getYaw());
+                getConfig().set("LobbySpawn.pitch", player.getLocation().getPitch());
+                player.sendMessage(ChatColor.GREEN + "LobbySpawn has been set!");
+            }
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/com/jaysn/bkc/Mechanics.java
+++ src/main/java/com/jaysn/bkc/Mechanics.java
@@ -1,6 +1,71 @@
 package com.jaysn.bkc;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Mechanics implements Listener{
+
+    private MainClass plugin = MainClass.getPlugin(MainClass.class);
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event){
+
+        Player p = event.getPlayer();
+
+        if (plugin.getConfig().contains("AllowJoinMessage")){
+            if (plugin.getConfig().getBoolean("AllowJoinMessage")){
+                p.sendMessage(ChatColor.GOLD.toString() + ChatColor.BOLD + "BetterKillCustomization Version 1.0 - JaySN");
+            }
+        }
+
+    }
+
+    @EventHandler
+    public void killBalance(EntityDamageEvent event){
+        if (event.getEntity() instanceof Player && event.getFinalDamage() >= ((Player) event.getEntity()).getHealth()){
+            Player player = (Player) event.getEntity();
+
+            if (plugin.getConfig().getBoolean("AllowCustomRespawn")) {
+                if (plugin.getConfig().contains("LobbySpawn")) {
+
+                    World world = Bukkit.getWorld(plugin.getConfig().getString("LobbySpawn.world"));
+                    double x = plugin.getConfig().getDouble("LobbySpawn.x");
+                    double y = plugin.getConfig().getDouble("LobbySpawn.y");
+                    double z = plugin.getConfig().getDouble("LobbySpawn.z");
+                    double yaw = plugin.getConfig().getDouble("LobbySpawn.yaw");
+                    double pitch = plugin.getConfig().getDouble("LobbySpawn.pitch");
+                    Location lobbyspawn = new Location(world, x, y, z, (float) yaw, (float) pitch);
+                    event.setCancelled(true);
+                    ((Player) event.getEntity()).setHealth(20);
+                    event.getEntity().teleport(lobbyspawn);
+                } else {
+
+                    player.sendMessage(ChatColor.RED + "&c You did not set the lobbyspawn");
+                }
+            }
+            Player killed = (Player) event.getEntity();
+            Player killer = killed.getKiller();
+
+            try {
+                MainClass.econ.depositPlayer(((Player) event.getEntity()).getKiller(), 10);
+
+            }catch(NoSuchMethodError e) {
+
+                MainClass.econ.depositPlayer(killed.getName(), 10);
+            }
+
+            if(MainClass.r.transactionSuccess()) {
+                killer.sendMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("KillMessage")));
+            }
+
+        }
+    }
+
 }
